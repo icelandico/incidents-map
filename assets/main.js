2a11a1484d7d159547ac153d1f13ef07e225bf9c
Michal Muszynski <m.muszy@gmail.com>
2019-06-10 19:35:48 +0200

    Change contributions

--- assets/main.js
+++ assets/main.js
@@ -2,5 +2,5 @@ const myMap = L.map("mapid").setView([44.95, -93.31], 14)
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">Stop Violence Uptown <> OpenStreetMap</a> contributors'
 }).addTo(myMap)
