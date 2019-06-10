f444b26df12719728952c336b0baadfe5050557d
Michal Muszynski <m.muszy@gmail.com>
2019-06-10 19:34:16 +0200

    Map config

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+const myMap = L.map("mapid").setView([44.95, -93.31], 14)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap)
