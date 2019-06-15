3249218f7b2f99d29a239872583e995fc0c616e9
Michal Muszynski <m.muszy@gmail.com>
2019-06-15 08:23:55 +0200

    Put functionalities into separate functions

--- assets/main.js
+++ assets/main.js
@@ -7,7 +7,7 @@ L.tileLayer(data.mapTiles, {
 }).addTo(myMap)
 
 const mapSet = {
-  data: "2019",
+  data: "2018",
   layer: "markers"
 }
 
@@ -30,17 +30,54 @@ function determineData() {
   }
 }
 
+function drawLayer(e) {
+  const { layer } = mapSet
+  switch (layer) {
+    case "markers":
+      drawMarkers(e)
+      break;
+    case "heatmap":
+      drawHeatmap(e)
+  }
+}
+
+function filterData(incidents) {
+  const points = changeCase(incidents.features)
+  const precinct = "5"
+  const pointsFiltered = points.filter(i => i.properties.precinct.includes(precinct))
+  return pointsFiltered
+}
+
+// Data from different time extent has different keys case in properties (i.e. ReportedDate vs reportedDate)
+// Function is supposed to marge it into one standard format
+
+function changeCase(array) {
+  const incidents = array.map((p, i) => {
+    const keys = Object.keys(p.properties)
+    let idx = keys.length
+    const newProps = {}
+    while (idx--) {
+      let key = keys[idx]
+      newProps[key.toLowerCase()] = p.properties[key]
+    }
+    return { ...p, properties: newProps }
+  })
+  return incidents
+}
+
 function drawMarkers(d) {
-  const points = d.features
-  const pointsFiltered = filterPrecinct(points)
+  const pointsFiltered = filterData(d)
   const coordinatesOnly = pointsFiltered.map(el => [el.geometry.coordinates[1], el.geometry.coordinates[0]])
-  // generateHeatmap(coordinatesOnly)
   pointsFiltered.map(e => placeMarker(e.geometry.coordinates, e.properties))
 }
 
+function drawHeatmap(d) {
+  generateHeatmap(coordinatesOnly)
+}
+
 function placeMarker(coordinates, properties) {
-  const info = properties.Description
-  const date = formatDate(properties.ReportedDate) || "Unknown"
+  const info = properties.description
+  const date = formatDate(properties.reporteddate) || "Unknown"
   const content = `<p>Description: ${info}</p><p>Reported date: ${date}</p>`
   if (coordinates.includes(null, 0)) {
     return null
@@ -61,32 +98,10 @@ function generateHeatmap(data) {
   L.heatLayer(data, { radius: 20 }).addTo(myMap)
 }
 
-
-function filterPrecinct(incidents) {
-  const precinct = "5"
-  const filtered = incidents.filter(i => i.properties.Precinct.includes("5"))
-  return filtered
-}
-
-function changeCase(array) {
-  const incidents = array.map((p, i) => {
-    const keys = Object.keys(p.properties)
-    let idx = keys.length
-    const newProps = {}
-    while (idx--) {
-      let key = keys[idx]
-      newProps[key.toLowerCase()] = p.properties[key]
-    }
-    return { ...p, properties: newProps }
-  }
-  )
-  return incidents
-}
-
 const input = document.querySelectorAll("label")
 
 input.forEach(el => el.addEventListener("click", function () {
   console.log(el.getAttribute("for"))
 }))
 
-request().then(e => drawMarkers(e))
+request().then(e => drawLayer(e))
