cf19461f1699cdb76713fe12637067dfa4ea6626
Michal Muszynski <m.muszy@gmail.com>
2019-06-16 11:29:01 +0200

    Attribution changed

--- assets/config.js
+++ assets/config.js
@@ -2,8 +2,8 @@ const data = {
   data2018: "https://opendata.arcgis.com/datasets/58e6f399e0f04c568b3ba45086d15818_0.geojson",
   data2019: "https://opendata.arcgis.com/datasets/8cd15449ac344aa5a55be7840d67c52d_0.geojson",
   mapZoom: 13,
-  mapTiles: "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png",
+  mapTiles: "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
   mapCenter: [44.94, -93.31],
-  mapAttribution: `&copy; <a href="https://www.openstreetmap.org/copyright">Stop Violence Uptown, OpenStreetMap</a> contributors`
+  mapAttribution: `Stop Violence, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
 }
 
